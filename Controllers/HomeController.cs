97ac1df3a79b51d6a95dba7e4b5ca5b29b9b2085
Shyamcode01 <shyamyadav121240@gmail.com>
2024-08-20 16:29:35 +0530

    add data

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using nmvUiproject.Models;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace nmvUiproject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private string url = "https://localhost:7201/api/Home";
+        private HttpClient _client= new HttpClient();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult create(studentregister obj)
+        {
+            string data= Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            StringContent content = new StringContent(data,Encoding.UTF8,"application/json");
+            HttpResponseMessage response=_client.PostAsync(url, content).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        //[HttpGet]
+        //public async Task< IActionResult> Index()
+        //{
+        //    try
+        //    {
+        //        List<studentregister> studentregisters = new List<studentregister>();
+        //        HttpResponseMessage response =await _client.GetAsync(url);
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            string result = await response.Content.ReadAsStringAsync();
+        //            var data = JsonConvert.DeserializeObject<List<studentregister>>(result);
+        //            if (data != null)
+        //            {
+        //                studentregisters = data;
+        //            }
+        //        }
+        //        return View(studentregisters);
+        //    }catch(Exception ex)
+        //    {
+        //        return View(ex.Message);
+        //    }
+        //}
+    }
+}
---
+++ Models/studentregister.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nmvUiproject.Models
+{
+    public class studentregister
+    {
+
+        [Required]
+            public int id { get; set; }
+        [Required]
+            public string name { get; set; }
+        [Required]
+            public int age { get; set; }
+        [Required]
+            public string gmail { get; set; }
+        
+
+    }
+}
